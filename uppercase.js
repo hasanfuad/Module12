a97a06e6593893915590339ea73f19a6274a963b
hasanfuad <hasanmdfuad@hotmail.com>
2021-06-10 03:14:43 +0600

    git commit js very very basic

---
+++ uppercase.js
@@ -0,0 +1,15 @@
+// Very basic part skipped
+//upperCase
+const uppercase = "HEY MAMA HEY"
+console.log(uppercase.toLowerCase());
+// indexof 
+const word = "mynameis fuad";
+console.log(word.indexOf("is"));
+console.log(word.split(" "));
+
+//parse float and int
+
+let num1 = 23;
+let num2 = '23';
+num2 = parseInt(num2)
+console.log(num1+num2);
